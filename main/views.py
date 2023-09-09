3c788de36dfde2cacdc81680194903fd9a8f5407
jchoi-projects <64113982+jchoi-projects@users.noreply.github.com>
2023-09-09 11:51:53 +1000

    mapped message tags to bootstrap

--- main/views.py
+++ main/views.py
@@ -10,31 +10,41 @@ def homepage_request(request):
 	return render(request=request, template_name='homepage.html')
 
 def register_request(request):
-	if request.method == 'POST':
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, 'Registration successful.' )
-			return redirect('main:homepage')
-		messages.error(request, 'Unsuccessful registration. Invalid information.')
-	form = NewUserForm()
+	if request.method != 'POST':
+		return render(
+			request = request,
+			template_name = 'register.html',
+			context = {'register_form':NewUserForm()}
+		)
+	
+	form = NewUserForm(request.POST)
+	if form.is_valid():
+		user = form.save()
+		login(request, user)
+		messages.success(request, 'Registration successful.' )
+		return redirect('main:homepage')
+	messages.error(request, 'Unsuccessful registration. Invalid information.')
 	return render(request=request, template_name='register.html', context={'register_form':form})
 
 def login_request(request):
-	if request.method == 'POST':
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f'You are now logged in as {username}.')
-				return redirect('main:homepage')
-			else:
-				messages.error(request,'Invalid username or password.')
+	if request.method != 'POST':
+		return render(
+			request = request,
+			template_name = 'login.html',
+			context = {'login_form': AuthenticationForm()}
+		)
+	
+	form = AuthenticationForm(request, data=request.POST)
+	if form.is_valid():
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.info(request, f'You are now logged in as {username}.')
+			return redirect('main:homepage')
 		else:
 			messages.error(request,'Invalid username or password.')
-	form = AuthenticationForm()
+	else:
+		messages.error(request,'Invalid username or password.')
 	return render(request=request, template_name='login.html', context={'login_form':form})
--- trailtracker/settings.py
+++ trailtracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
  
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Map message tags to Bootstrap
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
