e23dd2788692c833e8ee429e12c25cc125c581fd
johnjichoi <130121208+johnjichoi@users.noreply.github.com>
2023-09-10 17:02:44 +1000

    added photo upload (#3)
    Co-authored-by: jchoi-projects <64113982+jchoi-projects@users.noreply.github.com>

--- trip/urls.py
+++ trip/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add', views.add_request, name='add'),
     path('<int:id>/delete', views.delete_request, name='delete'),
     path('<int:id>/edit', views.edit_request, name='edit'),
+    path('<int:id>/upload', views.upload_request, name='upload'),
     path('<int:id>/view', views.view_request, name='view'),
 ]
